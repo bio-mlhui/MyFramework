aaf78fc9f41ade484a4ec2acc26908c6f3280586
huihui <huihui.mlpeople@gmail.com>
2023-10-08 11:17:05 +0800

    添加了drop

---
+++ models/pretrained_video_instance_decoder/pt_obj_decoder.py
@@ -0,0 +1,11 @@
+_pt_obj_decoder_entrypoints = {}
+def register_pt_obj_decoder(fn):
+    pt_obj_decoder_name = fn.__name__
+    _pt_obj_decoder_entrypoints[pt_obj_decoder_name] = fn
+
+    return fn
+def pt_obj_decoder_entrypoint(pt_obj_decoder_name):
+    try:
+        return _pt_obj_decoder_entrypoints[pt_obj_decoder_name]
+    except KeyError as e:
+        print(f'RVOS moel {pt_obj_decoder_name} not found')
