c9b510528d046d458d7633bdb2e4c2fea27b56bb
huihui <huihui.mlpeople@gmail.com>
2023-11-16 06:28:13 +0800

    graphv3测试有错

--- trainers/RVOS.py
+++ trainers/RVOS.py
@@ -132,7 +132,7 @@ class Trainer:
             
             epoch_header = f'Epoch[{self.epoch:{int(math.log10(self.total_epochs))+1}}/{self.total_epochs-1}]'
             debug_data_loding = False
-            debug_step_iteration = True
+            debug_step_iteration = False
             for idx, batch_dict in enumerate(self.train_loader):
                 if debug_data_loding:
                     continue
